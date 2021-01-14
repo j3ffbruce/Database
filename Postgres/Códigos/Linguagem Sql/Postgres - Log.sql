df901ff0fa8ca7ac672e7244b074612c76a813f2
Jefferson Alves <jeffersonalvessantos@hotmail.com>
2021-01-14 13:51:34 -0300

    (Feature) Registro de Log
    - Principais Parametros de LOG do Postgres
    
    ✔ logging_collector
    ✔ log_destination
    ✔ log_directory
    ✔ log_filename
    ✔ log_rotation_age
    ✔ log_rotation_size
    ✔ log_truncate_on_rotation
    ✔ log_truncate_on_rotation

---
+++ Postgres/Códigos/Linguagem Sql/Postgres - Log.sql
@@ -0,0 +1,33 @@
+-- ====================================================================================
+-- SGBD: <PostgreSQL>
+-- Autor: Jefferson Alves Santos - https://github.com/j3ffbruce
+-- Descrição: Configuração de Log no Postgres
+-- ====================================================================================
+
+-- Consulta Configuração -- 
+SELECT * FROM pg_settings 
+WHERE pg_settings.name ILIKE '<name_setting>';
+
+-- Realiza a ativação do LOG no Postgres -- 
+ALTER SYSTEM SET logging_collector = ON;
+
+-- Parametriza para que o LOG seja gerado em formato CSV (stderr é o padrão) --
+ALTER SYSTEM SET log_destination = 'csvlog';
+
+-- Parametriza O Diretório de Geração do LOG (pg_log é o padrão) --
+ALTER SYSTEM SET log_directory = 'pg_log';
+
+-- Parametriza O nome do arquivo de log a ser gerado (postgresql-%Y-%m-%d_%H%M%S.log é o padrão)  - 0 desativa a limitação --
+ALTER SYSTEM SET log_filename  = 'postgresql-%Y-%m-%d_%H%M%S.log';
+
+-- Parametriza o tempo de vida de um arquivo (1D é o padrão) --
+ALTER SYSTEM SET log_rotation_age  = '1D';
+
+-- Parametriza o tamanho maximo de um arquivo individual (10240kb é o padrão) - 0 desativa a limitação --
+ALTER SYSTEM SET log_rotation_size  = '1024';
+
+-- Parametriza a sobrescrição dos arquivos de log (Padrão = OFF)
+ALTER SYSTEM SET log_truncate_on_rotation  = 'off';
+
+-- Parametriza qual conteúdo será logado (Padrão = none) - ddl , mod e all --
+ALTER SYSTEM SET log_truncate_on_rotation  = 'off';
