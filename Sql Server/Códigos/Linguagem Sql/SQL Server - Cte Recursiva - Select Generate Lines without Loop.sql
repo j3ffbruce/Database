19dde299ffa1eccd0455b8a4fc27d60b715c4d85
Jefferson Alves Santos <jeffersonalvessantos@hotmail.com>
2021-05-31 15:42:41 -0300

    (Feature) Cte Recursiva
    ✔ Exemplo de Utilização de Cte Recursiva

---
+++ Sql Server/Códigos/Linguagem Sql/SQL Server - Cte Recursiva - Select Generate Lines without Loop.sql
@@ -0,0 +1,36 @@
+-- ====================================================================================
+-- SGBD: <SQL Server>
+-- Autor: Jefferson Alves Santos - https://github.com/j3ffbruce
+-- Descrição: CteRecursiva
+-- ====================================================================================
+
+
+-- EXEMPLO:
+
+-- X3 CONTRATOS
+-- X6 PARCELAS CADA
+-- X18 LINHAS
+
+
+
+DECLARE  @QTD_LINHAS INT, @QTD_CONTRATO INT, @QTD_PARCELA INT;
+
+
+SET @QTD_CONTRATO = 10;
+SET @QTD_PARCELA = 10;
+SET @QTD_LINHAS = @QTD_CONTRATO * @QTD_PARCELA;
+
+WITH T1 AS(
+	SELECT 1 AS CONTADOR_1, 1 AS CONTADOR_2
+	UNION ALL
+	SELECT CONTADOR_1 + 1,
+	CASE WHEN CONTADOR_1 % @QTD_PARCELA = 0
+	THEN CONTADOR_2 + 1 ELSE CONTADOR_2 
+	END 
+	FROM T1 WHERE CONTADOR_1 < @QTD_LINHAS 
+)
+	SELECT CONTADOR_2,
+		   RANK() OVER(PARTITION BY CONTADOR_2 ORDER BY CONTADOR_1 ) 
+	FROM T1
+
+
