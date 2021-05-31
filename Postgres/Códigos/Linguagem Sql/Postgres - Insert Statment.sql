64ad3d84e53d3577aee4b28f8b8193067d98345c
Jefferson Alves Santos <jeffersonalvessantos@hotmail.com>
2021-05-31 14:59:21 -0300

    (Feature) Statment Insert
    ✔ Centralização dos Recurso do Comando Insert em um único Arquivo
    - Gerador de Insert
    - Insert com Retorno

--- Postgres/Códigos/Linguagem Sql/Postgres - Insert Statment.sql
+++ Postgres/Códigos/Linguagem Sql/Postgres - Insert Statment.sql
@@ -1,10 +1,10 @@
 -- ====================================================================================
 -- SGBD: <PostgreSQL>
 -- Autor: Jefferson Alves Santos - https://github.com/j3ffbruce
--- Descrição: Gerador de Insert
+-- Descrição: Insert Statmanet
 -- ====================================================================================
 
-
+--- Gerador de Insert 
 SELECT 
 'INSERT INTO ' || table_name || ' (' || STRING_AGG(column_name,', ' ORDER BY NULL) || 
 ') VALUES ();'
@@ -12,3 +12,21 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE table_name = '<table_name>'
 GROUP BY table_name;
 
+--- Insert com Retorno
+DO $$
+DECLARE
+	var_column_01 varchar;
+	
+BEGIN
+	
+	INSERT INTO public."<table_name>"
+	SELECT uuid_generate_v4(), column_01, column_02
+	FROM public."<table_name>"
+	LIMIT 1
+	RETURNING column_01
+	INTO var_tb_banco;
+	RAISE INFO '- Valor da Column_01 %', var_column_01;
+	
+END;
+	
+$$ LANGUAGE PLPGSQL;
