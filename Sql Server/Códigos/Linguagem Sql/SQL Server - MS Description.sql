e5fb6802c171b9ae306a8a88f096737a10bba481
Jefferson Alves Santos <jeffersonalvessantos@hotmail.com>
2021-05-31 15:38:51 -0300

    (Feature) Description Objects
    ✔ Adicionada Querys de Exibição de Descrições de Tabelas
    ✔ Adicionada Querys de Exibição de Descrições de Colunas
    ✔ Adicionada Proc. de Adição de Descrição em Colunas
    ✔ Adicionada Proc. de Adição de Descrição em Tabelas
    ✔ Adicionada Proc. de Adição de Descrição em Funções

---
+++ Sql Server/Códigos/Linguagem Sql/SQL Server - MS Description.sql
@@ -0,0 +1,71 @@
+-- ====================================================================================
+-- SGBD: <SQL Server>
+-- Autor: Jefferson Alves Santos - https://github.com/j3ffbruce
+-- Descrição: Ms Description
+-- ====================================================================================
+
+
+-- Obtendo todas as descrições das colunas de uma Tabela
+
+SELECT u.name + '.' + t.name AS 'Schema/Table Names' ,
+       -- td.value AS 'Table Desc' ,
+       c.name AS 'Column Name' ,
+       cd.value AS 'Column Desc'
+FROM sysobjects AS t
+INNER JOIN sysusers AS u ON u.uid = t.uid
+LEFT OUTER JOIN sys.extended_properties AS td ON td.major_id = t.id
+       AND td.minor_id = 0
+       AND td.name = 'MS_Description'
+INNER JOIN syscolumns AS c ON c.id = t.id
+LEFT OUTER JOIN sys.extended_properties AS cd ON cd.major_id = c.id
+       AND cd.minor_id = c.colid
+       AND cd.name = 'MS_Description'
+WHERE t.type = 'u'
+       AND NOT cd.value IS NULL
+ORDER BY t.name, c.colorder
+ 
+-- Obtendo todas as descrições de uma Tabela
+SELECT c.name AS Field,
+       t.name AS Type,
+       c.Precision,
+       c.Scale,
+       c.is_nullable,
+       c.collation_name
+FROM sys.columns AS c
+INNER JOIN sys.types AS t ON t.system_type_id = c.system_type_id
+WHERE object_id = object_id('<insert_table_name>')
+ORDER BY column_id
+ 
+-- Adicionando Descrição em Tabela
+EXEC sys.sp_addextendedproperty
+     @name = N'MS_Description',
+     @value = N'Street address information for customers, employees, and vendors.',
+     @level0type = N'SCHEMA', @level0name = 'Person',
+     @level1type = N'TABLE', @level1name = 'Address';
+GO
+ 
+-- Adicionando Descrição em Coluna
+EXEC sys.sp_addextendedproperty
+     @name = N'MS_Description',
+     @value = 'Postal code is a required column.',
+     @level0type = N'SCHEMA', @level0name = 'Person',
+     @level1type = N'TABLE', @level1name = 'Address',
+     @level2type = N'COLUMN', @level2name = 'PostalCode';
+GO
+ 
+-- Atualizando Descrição em uma Coluna
+EXEC sys.sp_updateextendedproperty
+     @name = N'MS_Description' ,
+     @value = 'Employee ID must be unique.' ,
+     @level0type = N'SCHEMA', @level0name = 'dbo' ,
+     @level1type = N'TABLE', @level1name = 'T1' ,
+     @level2type = N'COLUMN', @level2name = 'id';
+GO
+ 
+-- Atualizando Descrição de uma Função
+EXEC sys.sp_addextendedproperty
+     @name = N'MS_Description',
+     @value = N'Performs LTRIM(RTRIM()) on input string. Optionally loops to convert all double-spaces to single-spaces.',
+     @level0type = N'SCHEMA', @level0name = 'dbo',
+     @level1type = N'FUNCTION', @level1name = 'fn_Trim';
+GO
