470c587e9b7d84504f53f319485b7b5dc8080b9c
Jefferson Alves Santos <jeffersonalvessantos@hotmail.com>
2021-08-24 09:08:53 -0300

    (Fix) Bloco de Programação T-SQL
    1. Padronização dos Comentários do Código em Inglês
    2. Correção do nome do SGBD no Cabeçalho do Script

--- Sql Server/Códigos/Linguagem Sql/SQL Server - Estrutura de Script T-SQL.sql
+++ Sql Server/Códigos/Linguagem Sql/SQL Server - Estrutura de Script T-SQL.sql
@@ -1,32 +1,32 @@
 -- ====================================================================================
--- SGBD: <PostgreSQL>
--- Autor: Jefferson Alves Santos - https://github.com/j3ffbruce
--- Descrição: Bloco de Programação T-SQL
+-- SGBD: <SQL Server>
+-- Author: Jefferson Alves Santos - https://github.com/j3ffbruce
+-- Description: Programming Block T-SQL
 -- ====================================================================================
 
 SET TRANSACTION ISOLATION LEVEL SERIALIZABLE 
 
 BEGIN TRY
 	
-	PRINT CHAR(13) + N'#Iniciando Script' + CHAR(13);
+	PRINT CHAR(13) + N'#Starting Script' + CHAR(13);
 
-	PRINT N'- Configurando sessao para execucao do script ...';
+	PRINT N'- Configuring Session';
 	SET DATEFORMAT ymd;
 	SET NOCOUNT ON;
 	SET XACT_ABORT ON;
 	
-	PRINT N'- Abrindo transação ...';
+	PRINT N'- Opening transaction ...';
 	BEGIN TRAN
 
 	
 	-- ;THROW 51000, '- Message ', 1;  
 	
 	
-	PRINT N'- Script Executado c/ Sucesso, efetuar o COMMIT !';
+	PRINT N'- Script Executed Successfully, please commit !';
 
 END TRY
 BEGIN CATCH
-	PRINT CHAR(13) + N'#Script Erro - Alterações Revertidas !';
+	PRINT CHAR(13) + N'# Script Run with Error, changes undone - ROLLBACK!';
 	THROW;
 	IF @@TRANCOUNT > 0 ROLLBACK 
 END CATCH	
