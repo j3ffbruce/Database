8c6f22d92c13ddc671d1310c6f08687bfb205de0
Jefferson Alves Santos <jeffersonalvessantos@hotmail.com>
2021-10-07 09:11:13 -0300

    (Feature) Bloco de Programação T-SQL
    + Melhoria no Log de Mensagens: Log em Tempo de Execução

--- Sql Server/Códigos/Linguagem Sql/SQL Server - Estrutura de Script T-SQL.sql
+++ Sql Server/Códigos/Linguagem Sql/SQL Server - Estrutura de Script T-SQL.sql
@@ -2,31 +2,46 @@
 -- DBMS: <SQL Server>
 -- Author: Jefferson Alves Santos - https://github.com/j3ffbruce
 -- Description: Programming Block T-SQL
+-- Keywords: T-SQL, Block Programing
 -- ====================================================================================
+
+-- SQL STRUCTURED QUERY LANGUGAGE) ----------------------------------------------------
+
+-- Runtime (Server\Database): 00m:00s
 
 SET TRANSACTION ISOLATION LEVEL SERIALIZABLE 
 
 BEGIN TRY
 	
-	PRINT CHAR(13) + N'#Starting Script' + CHAR(13);
-
-	PRINT N'- Configuring Session';
+	DECLARE 
+	@MSG VARCHAR(MAX),
+	@STEP INT = 0;
+			
+	SET @STEP = @STEP + 1;
+	SET @MSG = CHAR(13) + '[Step: ' + CAST(@STEP AS VARCHAR) + ' -' + CONVERT(VARCHAR,GETDATE(),21) + '] Starting Script'
+	RAISERROR(@MSG,0,1) WITH NOWAIT;
+		
 	SET DATEFORMAT ymd;
 	SET NOCOUNT ON;
 	SET XACT_ABORT ON;
 	
-	PRINT N'- Opening transaction ...';
+	SET @STEP = @STEP + 1;
+	SET @MSG = '[Step: ' + CAST(@STEP AS VARCHAR) + ' -' + CONVERT(VARCHAR,GETDATE(),21) + '] Opening Transaction'
+	RAISERROR(@MSG,0,1) WITH NOWAIT;
+		
 	BEGIN TRAN
-
-	
+		
 	-- ;THROW 51000, '- Message ', 1;  
 	
+	SET @STEP = @STEP + 1;
+	SET @MSG = '[Step: ' + CAST(@STEP AS VARCHAR) + ' -' + CONVERT(VARCHAR,GETDATE(),21) + '] Script Finished'
+	RAISERROR(@MSG,0,1) WITH NOWAIT;
 	
-	PRINT N'- Script Executed Successfully, please commit !';
-
+	-- COMMIT
+	
 END TRY
 BEGIN CATCH
-	PRINT CHAR(13) + N'# Script Run with Error, changes undone - ROLLBACK!';
+	PRINT CHAR(13) + N'Error: Script Run with Error, changes undone - ROLLBACK!';
 	THROW;
 	IF @@TRANCOUNT > 0 ROLLBACK 
 END CATCH	
